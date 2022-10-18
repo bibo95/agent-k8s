eafa1b22c39ca9b2caeec2b3d553ca835682adb6
bibo95 <habib.abdelkrim@gmail.com>
2022-10-18 23:30:33 +0200

    Update autosys.tf

--- autosys.tf
+++ autosys.tf
@@ -16,43 +16,43 @@ resource "kubernetes_manifest" "autosys_agent" {
   
   manifest = {
     
-    apiVersion = "autosys.ddp.bp2i/v1alpha1"
-    kind       = "AutosysAgent"
+    "apiVersion" = "autosys.ddp.bp2i/v1alpha1"
+    "kind"       = "AutosysAgent"
 
-    metadata = {
-      name      = "autosys-agent-${var.namespace}"
-      namespace = "${var.namespace}"
+    "metadata" = {
+      "name"      = "autosys-agent-${var.namespace}"
+      "namespace" = "${var.namespace}"
     }
 
-    spec = {
-      contact = "paris_bp2i_scheduling_products_open_at_bnpparibas.com"
-      identification = {
-        appcode = "AP24664"
-        codeap = "AP24664"
-        ecosystem = "toto"
-        tier = "PA"    
+    "spec" = {
+      "contact" = "paris_bp2i_scheduling_products_open_at_bnpparibas.com"
+      "identification" = {
+        "appcode" = "AP24664"
+        "codeap" = "AP24664"
+        "ecosystem" = "toto"
+        "tier" = "PA"    
       }
-      illumio = {
-        app = "A_toto-CA-AUTOSYS_0-DEFAULT"
-        env = "E_DEV"
-        loc = "L_EMEA_T2_BNPP_DMZR-VPC-BIZ"
-        role = "R_RESTRICTED_AD_KUBE_OTHER-SOFT"
+      "illumio" = {
+        "app" = "A_toto-CA-AUTOSYS_0-DEFAULT"
+        "env" = "E_DEV"
+        "loc" = "L_EMEA_T2_BNPP_DMZR-VPC-BIZ"
+        "role" = "R_RESTRICTED_AD_KUBE_OTHER-SOFT"
       }
-      resources = {
-          limits = {
-            cpu = "200m"
-            memory = "256Mi"
+      "resources" = {
+          "limits" = {
+            "cpu" = "200m"
+            "memory" = "256Mi"
           }
-          requests = {
-            cpu = "50m"
-            memory = "128Mi"
+          "requests" = {
+            "cpu" = "50m"
+            "memory" = "128Mi"
           }
       } 
-      splunk = {
-        index = "cloud_bp2i_autosys"
-        source = "all.autosys.json"
+      "splunk" = {
+        "index" = "cloud_bp2i_autosys"
+        "source" = "all.autosys.json"
       }
-      scope = "Namespaced"
+      "scope" = "Namespaced"
     }
     
   }
