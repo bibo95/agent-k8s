7debd0ddb1ac320a49b91f0d1f8d811417a8512a
bibo95 <habib.abdelkrim@gmail.com>
2022-10-18 22:35:36 +0200

    Update autosys.tf

--- autosys.tf
+++ autosys.tf
@@ -1,1 +1,18 @@
 
+resource "kubernetes_config_map" "autosys_configmap" {
+  metadata {
+    name      = "autosys-config"
+    namespace = var.namespace
+  }
+
+  data = {
+    AUTOSYS_NAMESPACE          = var.namespace
+    AUTOSYS_APPCODE          = var.appcode
+    AUTOSYS_ECOSYSTEM     = var.ecosystem
+    AUTOSYS_ENVIRONMENT = var.environment
+  }
+
+
+}
+
+
